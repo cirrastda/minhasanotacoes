e9d88666177918234c6a71985e675b94b35dc3b5
Anderson Matuchenko <anderson.matuchenko@buonny.com.br>
2020-05-27 08:59:02 -0300

    Aplicativo Concluido

--- minhasanotacoes/ViewController.swift
+++ minhasanotacoes/ViewController.swift
@@ -10,11 +10,21 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var txtMensagem: UITextView!
     override func viewDidLoad() {
         super.viewDidLoad()
+        if let texto = UserDefaults.standard.object(forKey: "mensagem") {
+            txtMensagem.text = texto as! String
+        } else {
+            txtMensagem.text = ""
+        }
         // Do any additional setup after loading the view.
     }
 
 
+    @IBAction func btnSalvar_Click(_ sender: Any) {
+        UserDefaults.standard.set(txtMensagem.text, forKey: "mensagem")
+    }
 }
 
